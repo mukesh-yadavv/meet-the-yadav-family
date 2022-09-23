ccb48b24a40a1f4cc9149b71039b4b9d6eb6abad
unknown <mail.mukeshyadav@gmail.com>
2022-09-23 22:45:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,179 @@
+import logo from './logo.svg';
+import './App.css';
+// import family from "../../public/pics/family.jpg";
+
+import familyPic from './pics/family.jpg';
+import yadav_family from './pics/yadav_family.jpg';
+import familyHierachyPic from './pics/yadav_family_hierarchy1.JPG'
+import yadav_family_2 from './pics/yadav_family_2.jpg';
+import yadav_family_3 from './pics/yadav_family_3.jpg';
+import family1 from './pics/family1.jpg';
+import family2 from './pics/family2.jpg';
+import family3 from './pics/family3.jpg';
+import family4 from './pics/family4.jpg';
+import family5 from './pics/family5.jpg';
+import family6 from './pics/family6.jpg';
+import family7 from './pics/family7.jpg';
+import family8 from './pics/family8.jpg';
+import sunita_family_1 from './pics/sunita_family_1.jpg';
+import kids from './pics/kids.jpg';
+import familyFriends1 from './pics/familyFriends1.jpg';
+import familyFriends2 from './pics/familyFriends2.jpg';
+import familyFriends3 from './pics/familyFriends3.jpg';
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    {/* */}
+    {/* */}
+
+    <h1>Welcome to Yadav's Family Homepage.</h1>
+
+    <div className="wrapper">
+      <div className="item">
+        <img
+          src={familyPic}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src={yadav_family}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={familyHierachyPic}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family1}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family2}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family3}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={sunita_family_1}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family4}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family5}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family6}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={family7}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src={family8}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src={kids}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={yadav_family_2}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={yadav_family_3}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>      
+      <div className="item">
+        <img
+          src={familyFriends3}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={familyFriends1}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+      <div className="item">
+        <img
+          src={familyFriends2}
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>
+    </div>  
+
+
+     {/*   
+     <div className="item">
+        <img
+          src="pics/yadav_family_1.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div>         
+      <div className="item">
+        <img
+          src="pics/siblings.jpg"
+          alt="Yadav Family"
+          style={{ borderColor: 'black' }}
+        />
+      </div> 
+     */}   
+    
+  </div>
+)
